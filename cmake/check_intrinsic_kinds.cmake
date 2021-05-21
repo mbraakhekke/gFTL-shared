3b9c611caea81cb3959c43512189ebde49874ddc
Maarten Braakhekke <maarten.braakhekke@wur.nl>
2021-05-21 10:54:11 +0200

    Results from running trial sources during CMake config are now stored in the CMake cache

--- cmake/check_intrinsic_kinds.cmake
+++ cmake/check_intrinsic_kinds.cmake
@@ -3,19 +3,23 @@ include (fortran_try)
 CHECK_Fortran_SOURCE_RUN (
   ${CMAKE_CURRENT_LIST_DIR}/trial_sources/LOGICAL_DEFAULT_KIND.F90
   _LOGICAL_DEFAULT_KIND
+  "Default logical kind"
   )
 
 CHECK_Fortran_SOURCE_RUN (
   ${CMAKE_CURRENT_LIST_DIR}/trial_sources/INT_DEFAULT_KIND.F90
   _INT_DEFAULT_KIND
+  "Default integer kind"
   )
 foreach (kind 8 16 32 64)
+
   set(CMAKE_REQUIRED_FLAGS = -fpp)
   set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=INT${kind})
 
   CHECK_Fortran_SOURCE_RUN (
     ${CMAKE_CURRENT_LIST_DIR}/trial_sources/INT_KIND.F90
     _ISO_INT${kind}
+    "Integer(${kind}) kind"
     )
 
 endforeach()
@@ -23,37 +27,49 @@ endforeach()
 CHECK_Fortran_SOURCE_RUN (
   ${CMAKE_CURRENT_LIST_DIR}/trial_sources/REAL_DEFAULT_KIND.F90
   _REAL_DEFAULT_KIND
+  "Default real kind"
   )
 
 CHECK_Fortran_SOURCE_RUN (
   ${CMAKE_CURRENT_LIST_DIR}/trial_sources/DOUBLE_DEFAULT_KIND.F90
   _DOUBLE_DEFAULT_KIND
+  "Default double kind"
   )
 
 foreach (kind 32 64 128)
-  set(CMAKE_REQUIRED_FLAGS = -fpp)
-  set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=REAL${kind})
 
-  try_compile (
-    code_compiles
-    ${GFTL_SHARED_BINARY_DIR}
-    ${CMAKE_CURRENT_LIST_DIR}/trial_sources/REAL_KIND.F90
-    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+  if (DEFINED CACHE{GFTL_SHARED_ISO_REAL${kind}})
   
-  if (code_compiles)
-    CHECK_Fortran_SOURCE_RUN (
+	set(_ISO_REAL${kind} ${GFTL_SHARED_ISO_REAL${kind}})
+    add_definitions(-D_ISO_REAL${kind}=${_ISO_REAL${kind}})
+	  
+  else ()
+  
+    set(CMAKE_REQUIRED_FLAGS = -fpp)
+    set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=REAL${kind})
+
+    try_compile (
+      code_compiles
+      ${GFTL_SHARED_BINARY_DIR}
       ${CMAKE_CURRENT_LIST_DIR}/trial_sources/REAL_KIND.F90
-      _ISO_REAL${kind}
-      )
+      CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+    
+    if (code_compiles)
+      CHECK_Fortran_SOURCE_RUN (
+        ${CMAKE_CURRENT_LIST_DIR}/trial_sources/REAL_KIND.F90
+        _ISO_REAL${kind}
+        "Real(${kind}) kind"
+        )
+    endif ()
+    
+    try_compile (
+      code_compiles
+      ${GFTL_SHARED_BINARY_DIR}
+      ${CMAKE_CURRENT_LIST_DIR}/trial_sources/REAL_KIND.F90
+      CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+    
   endif ()
   
-  try_compile (
-    code_compiles
-    ${GFTL_SHARED_BINARY_DIR}
-    ${CMAKE_CURRENT_LIST_DIR}/trial_sources/REAL_KIND.F90
-    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
-  
-
 endforeach()
 
 
